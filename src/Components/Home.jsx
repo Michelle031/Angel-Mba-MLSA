33ef43d02323348f6a00eaf9173b843b73e254d1
Angel Mba <70325824+Michelle031@users.noreply.github.com>
2023-12-29 21:44:20 +0000

    final-change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/img0.jpg";
+import image from "../images/img3.jpg";
 
 const imageAltText = "Adult female smiling for a picture while working";
 
